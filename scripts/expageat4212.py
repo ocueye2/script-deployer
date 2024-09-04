9dda01d5701cf1b53fff3646555f03f8252ea5f2
ocueye2 <carson@carsonmayn.com>
2024-09-04 14:56:15 +0000

    edited example

--- scripts/expageat4212.py
+++ scripts/expageat4212.py
@@ -3,7 +3,7 @@ import cherrypy
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return "test"
+        return "This is an exampe service running at 4212"
 cherrypy.config.update({
         'server.socket_port': 4212,
         'log.screen': True  # Disable console logging
